9f91d3718efcdfdacdcab308488767e69ee6909f
JohnDoeAntler <16289028+JohnDoeAntler@users.noreply.github.com>
2022-01-29 18:28:33 +0800

    feat: extract interface of newscard components

--- src/components/NewsCard/index.tsx
+++ src/components/NewsCard/index.tsx
@@ -14,7 +14,26 @@ import BookmarkIcon from '@mui/icons-material/Bookmark';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import Divider from "@mui/material/Divider";
 
-const NewsCard = () => {
+interface INewsCardProps {
+
+    // for passing the reference for rating and bookmarking.
+    news_id: string;
+
+    title: string;
+
+    description: string;
+
+    date: string;
+
+    source_url: string;
+
+    source_icon_url: string;
+
+    source_name: string;
+
+}
+
+const NewsCard = (props: INewsCardProps) => {
 
     const keywords = "amet sequi deleniti quam explicabo quia unde necessitatibus saepe labore rerum".split(" ");
 
@@ -23,62 +42,67 @@ const NewsCard = () => {
     }, [keywords]);
 
     return (
-        <Card sx={{ minWidth: 275, maxWidth: 500, borderLeft: ".7rem solid #88cc88", borderRadius: ".5rem" }} elevation={4}>
-            <CardContent>
-                <Typography sx={{ fontWeight: 600}}>
-                    Word of the Day
-                </Typography>
+        <a href={props.source_url} style={{ textDecoration: "unset" }}>
+            <Card sx={{ minWidth: 275, maxWidth: 500, borderLeft: ".7rem solid #88cc88", borderRadius: ".5rem" }
+            } elevation={4} >
+                <CardContent>
+                    <Typography sx={{ fontWeight: 600 }}>
+                        {props.title}
+                    </Typography>
 
-                <Typography variant="body1" color="text.secondary">
-                    4 hours ago
-                </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                        {props.date}
+                    </Typography>
 
-                <Box sx={{ my: "1rem" }}></Box>
+                    <Box sx={{ my: "1rem" }}></Box>
 
-                <Typography color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione maxime praesentium dolores id corrupti nihil ex, deleniti eum laboriosam sit beatae, neque saepe nesciunt ea nemo qui incidunt eaque quas.
-                </Typography>
+                    <Typography color="text.secondary">
+                        {props.description}
+                    </Typography>
 
-                <Stack columnGap=".5rem" rowGap=".5rem" flexWrap="wrap" flexDirection="row" sx={{ mt: ".5rem" }}>
-                    {
-                        tags
-                    }
-                </Stack>
-            </CardContent>
-
-            <Divider sx={{my:"1rem"}}></Divider>
-
-            <CardActions>
-                <Stack flexDirection="row" justifyContent="space-between" alignItems="center" sx={{ width: "100%" }}>
-                    <Stack flexDirection="row" alignItems="center" sx={{ pl: ".5rem" }} columnGap=".5rem">
-                        <Avatar 
-                            alt="Travis Howard"
-                            src="https://source.unsplash.com/random/400x200" 
-                            sx={{
-                                width: "1.5rem",
-                                height: "1.5rem",
-                            }}
-                        />
-
-                        <Typography>
-                            Word of the Day
-                        </Typography>
+                    <Stack columnGap=".5rem" rowGap=".5rem" flexWrap="wrap" flexDirection="row" sx={{ mt: ".5rem" }}>
+                        {
+                            tags
+                        }
                     </Stack>
+                </CardContent>
 
-                    <Stack flexDirection="row" columnGap=".5rem">
-                        <IconButton>
-                            <AddCommentIcon />
-                        </IconButton>
-                        <IconButton>
-                            <BookmarkIcon />
-                        </IconButton>
-                        <IconButton>
-                            <FilterAltIcon />
-                        </IconButton>
+                <Divider sx={{ my: "1rem" }}></Divider>
+
+                <CardActions>
+                    <Stack flexDirection="row" justifyContent="space-between" alignItems="center" sx={{ width: "100%" }}>
+                        <Stack flexDirection="row" alignItems="center" sx={{ pl: ".5rem" }} columnGap=".5rem">
+                            <Avatar
+                                alt="Travis Howard"
+                                src={props.source_icon_url}
+                                sx={{
+                                    width: "1.5rem",
+                                    height: "1.5rem",
+                                }}
+                            />
+
+                            <Typography>
+                                {
+                                    props.source_name
+                                }
+                            </Typography>
+                        </Stack>
+
+                        <Stack flexDirection="row" columnGap=".5rem">
+                            <IconButton>
+                                <AddCommentIcon />
+                            </IconButton>
+                            <IconButton>
+                                <BookmarkIcon />
+                            </IconButton>
+                            <IconButton>
+                                <FilterAltIcon />
+                            </IconButton>
+                        </Stack>
                     </Stack>
-                </Stack>
-            </CardActions>
-        </Card>
+                </CardActions>
+            </Card >
+        </a >
     );
 }
 
--- src/pages/oscar/index.tsx
+++ src/pages/oscar/index.tsx
@@ -6,7 +6,15 @@ const OscarPage = () => {
     return (
         <div className="oscar-page">
             <Container>
-                <NewsCard />
+                <NewsCard
+                    news_id="random_id"
+                    title="John Doe Antler"
+                    description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur repellat officia adipisci earum soluta, qui cumque cum odit neque in quasi aut delectus praesentium esse similique eaque corrupti incidunt eligendi!"
+                    date="4 hours ago"
+                    source_url="www.google.com"
+                    source_icon_url="https://source.unsplash.com/random/400x200"
+                    source_name="John Doe"
+                 />
             </Container>
         </div>
     );
