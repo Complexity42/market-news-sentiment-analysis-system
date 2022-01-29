f95ace79856c7e29ebf141ebd531730424141724
Jerrylum <serverofjerry@gmail.com>
2022-01-29 21:23:12 +0800

    build landing page

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,10 +1,10 @@
 import { Stack, Typography } from "@mui/material";
 import { useState } from "react";
 
-export function Title(props: { title: string, show_all: boolean, children: React.ReactNode }) {
+export function Title(props: { title: string, show_all: boolean, children: React.ReactNode, can_hide?: boolean }) {
 	const [show_all, setShowAll] = useState(props.show_all);
 	return (
-		<div style={{ marginBottom: '1.5rem' }}>
+		<div style={{ marginBottom: '1.7rem' }}>
 			<Stack
 				direction="row"
 				justifyContent="space-between"
@@ -13,13 +13,13 @@ export function Title(props: { title: string, show_all: boolean, children: React
 				<Typography variant="h5" component="h5" sx={{ width: '2.5rem', borderBottom: 'solid #CCC 1px', lineHeight: '0.1rem' }}></Typography>
 				<Typography variant="h5" component="h5" sx={{ ml: '0.7rem', mr: '0.7rem', }}>{props.title}</Typography>
 				<Typography variant="h5" component="h5" flexGrow="1" sx={{ borderBottom: 'solid #CCC 1px', lineHeight: '0.1rem' }}></Typography>
-				<Typography
+				{ props.can_hide ? <Typography
 					variant="body2"
 					component="h5"
 					sx={{ ml: '0.5rem', "&:hover": { color: 'dodgerblue', cursor: 'pointer' } }}
 					onClick={() => setShowAll(!show_all)}>
 					{show_all ? 'Hide all' : 'Show all'}
-				</Typography>
+				</Typography> : null }
 			</Stack>
 			{show_all ? props.children : null}
 		</div>
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -3,30 +3,108 @@ import "./index.css";
 import { observer } from "mobx-react";
 import CounterStore from "../../mobx/CounterStore";
 import { Container, Stack } from "@mui/material";
+import { Card, CardContent } from "@mui/material";
+import { useState } from "react";
+import { CommentCard } from "../../components/CommentCard";
+import { KeywordItem } from "../../components/KeywordItem";
+import LoginButton from "../../components/LoginButton";
+import { MyProfileCard } from "../../components/MyProfileCard";
+import { ProfilePreviewCard } from "../../components/ProfilePreviewCard";
+import { Title } from "../../components/Title";
 
 const LandingPage = observer(() => {
+	let user = {
+		name: "Giorgio",
+		bio: "My name is Giovanni Giorgio, but everybody calls me Giorgio",
+		created_at: "2020-01-01",
+		updated_at: "2020-01-01",
+		avatar: "https://source.unsplash.com/random/400x200",
+		uuid: "123",
+		subscribed_keywords: ["react", "typescript", "javascript"]
+	};
+
+	let comment = {
+		updated_at: "2022-1-19 11:45",
+		news_title: "【美股收市】蘋果績優飆7% 帶動道指倒升564點 納指彈逾3%",
+		news_id: "123",
+		news_source_url: "https://www.google.com",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+	}
+
+	let keyword_c = {
+		id: "react",
+		content: 'React',
+		is_blocked: false,
+		is_subscribed: true
+	}
+
+	// usestate keyword
+	const [keyword, setKeyword] = useState(keyword_c);
+
+	let controller = {
+		changeKeywordUserPreferences: (id: string, is_blocked: boolean, is_subscribed: boolean) => {
+			setKeyword({
+				...keyword,
+				is_blocked: is_blocked,
+				is_subscribed: is_subscribed
+			});
+		}
+	}
+
 	return (
 
 		<div>
-			<Container maxWidth="md">
-				<Stack direction="row" sx={{ backgroundColor: '#FCFCFC', height: '100vh' }}>
+			<Container>
+				<Stack direction="row" sx={{ backgroundColor: 'white', height: '100vh', pt: '2rem' }} justifyContent="center">
 					<Container
 						className="landing-page-container"
 						sx={{
 							backgroundColor: 'white'
 						}}>
-						<h1>Landing Page</h1>
-						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />e
+						<MyProfileCard data={user}></MyProfileCard>
 					</Container>
 					<Container
 						className="landing-page-container"
 						sx={{ backgroundColor: 'white' }}>
-						<h1>Landing Page</h1>
-						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />f
+						<Title title="Profile" show_all>
+							<MyProfileCard data={user}></MyProfileCard>
+						</Title>
+						<Title title="Subscribed" show_all>
+							<Card sx={{ minWidth: 275, maxWidth: 500, borderRadius: '0.5rem' }} elevation={4}>
+								<CardContent sx={{ p: '0.6rem 0.5rem 0.6rem 1.4rem !important' }}>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+								</CardContent>
+							</Card>
+						</Title>
+						<Title title="Not Interested" show_all>
+							<Card sx={{ minWidth: 275, maxWidth: 500, borderRadius: '0.5rem' }} elevation={4}>
+								<CardContent sx={{ p: '0.6rem 0.5rem 0.6rem 1.4rem !important' }}>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+								</CardContent>
+							</Card>
+						</Title>
+						<Title title="Suggestion" show_all>
+							<Card sx={{ minWidth: 275, maxWidth: 500, borderRadius: '0.5rem' }} elevation={4}>
+								<CardContent sx={{ p: '0.6rem 0.5rem 0.6rem 1.4rem !important' }}>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+									<KeywordItem data={keyword} controller={controller}></KeywordItem>
+								</CardContent>
+							</Card>
+						</Title>
 					</Container>
 				</Stack>
 			</Container >
